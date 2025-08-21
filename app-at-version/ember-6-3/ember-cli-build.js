1b06ddd159358a68b476ac8cf8c07135711bbfa0
David Taylor <david@taylorhq.com>
2025-08-21 14:34:41 +0100

    Add Ember 6.3

---
+++ app-at-version/ember-6-3/ember-cli-build.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = async function (defaults) {
+  const utils = await import('ember-cli-utils');
+  const config = await utils.configure(__dirname, ['common']);
+
+  const app = new EmberApp(defaults, {
+    ...config,
+  });
+
+  const { Webpack } = require('@embroider/webpack');
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+    staticEmberSource: true,
+    staticInvokables: true,
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+  });
+};
